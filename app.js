29038bf9bdac93d4432ac0f733b04ff9b1f420d0
Vishvam <s.vishvam1025@gmail.com>
2021-03-18 18:39:38 +0530

    Added Older Browser Support

--- app.js
+++ app.js
@@ -14,23 +14,56 @@ function rgbToHex(r, g, b) {
 const copyIcon = document.querySelectorAll(".copy");
 var container = document.querySelector('.container');
 var cardContainer = document.querySelector('.card-container');
+var utils = document.querySelector('.utilities');
+const nav = document.querySelector(".navigation");
+const navContainer = document.querySelector(".nav-container");
+var addGradient = document.querySelector('.add-gradient')
+var gradientForm = addGradient.childNodes[3];
+console.log(gradientForm);
+const darkSwitch = navContainer.childNodes[5];
+
+// var bookmarkTab = document.querySelector('.bookmark-tab')
+// var btCards = document.querySelectorAll('.bookmark-tab-card');
+
 var i;
+
+function oldBrowserSupport(condition, style) {
+    if(condition == "true") {
+        var input = document.getElementById('myInput').value = 
+        `background: ${style};
+        \nbackground: -webkit-${style};
+        \nbackground: -moz-${style};
+        \nbackground: -ms-${style};
+        \nbackground: -o-${style};
+        `
+    } else {
+        var input = document.getElementById('myInput').value = `background: ${style}`;
+    }
+    return input;
+}
+
+function copyCSS(e, condition) {
+    var trigger = e.target.dataset.number;
+    const gradient = document.getElementById(`gradient-${trigger}`);
+    const styleCSS = window.getComputedStyle(gradient).backgroundImage;
+
+    var inp = oldBrowserSupport(condition, styleCSS)
+    console.log(inp);
+
+    document.getElementById("inpbtn").click();  
+    var copied = document.querySelectorAll(".copied");
+    copied[trigger-1].classList.toggle("hide")
+    setTimeout(function() {
+        var copied = document.querySelectorAll(".copied");
+        copied[trigger-1].classList.toggle("hide");
+    }, 750);
+}
 
 for(i=0; i< copyIcon.length; i++) {
     copyIcon[i].addEventListener("click", e => {
-        var trigger = e.target.dataset.number;
-        const gradient = document.getElementById(`gradient-${trigger}`);
-        const styleCSS = window.getComputedStyle(gradient).backgroundImage;
-        var inp = document.getElementById('myInput').value = `background: ${styleCSS},\nbackground: -webkit-${styleCSS}`;
-        console.log(inp);
-        document.getElementById("inpbtn").click();  
-        var copied = document.querySelectorAll(".copied");
-        copied[trigger-1].classList.toggle("hide")
-        setTimeout(function() {
-            var copied = document.querySelectorAll(".copied");
-            copied[trigger-1].classList.toggle("hide");
-        }, 750);
-    });
+        var condition = navContainer.childNodes[3].dataset.comp;
+        copyCSS(e, condition);
+    })
 }
 
 function copyToClipboard() {
@@ -47,13 +80,12 @@ const allCards = document.querySelectorAll(".card")
 function gradient(e) {
     var color1 = e.childNodes[7].childNodes[1].style.backgroundColor;
     var color2 = e.childNodes[7].childNodes[3].style.backgroundColor;
-    e.style.boxShadow = `-.1rem -.1rem 1.6rem ${color1}, .1rem .1rem 1.6rem ${color2}`
+    e.style.boxShadow = `-.1rem -.1rem 1.2rem ${color1}, .1rem .1rem 1.2rem ${color2}`;
 }
 
 function nogradient(e) {
     e.style.boxShadow = "0 0.4rem 0.6rem rgba(50, 50, 50, 0.3)";
 }
-
 
 // UPDATE THE CIRCLES
 
@@ -64,16 +96,15 @@ for(i=0; i<allCards.length; i++) {
     var gradients = allCards[i].firstElementChild.children[0];
 
     var gradientsCSS = window.getComputedStyle(gradients).backgroundImage;
-    console.log(gradientsCSS);
     var c1 = gradientsCSS.split('rgb')[1].slice(0, -2);
     var c2 = gradientsCSS.split('rgb')[2].slice(0, -1);
-    console.log(c1);
+
     // FOR RGB VALUES
     
     var str = "rgba"
     var color1 = str.concat(c1); 
     var color2 = str.concat(c2); 
-    console.log(color1);
+
     var rgbColors = [color1, color2]
 
     var numberOfCircles = allCards[i].children[3].childElementCount;
@@ -93,10 +124,6 @@ for(i=0; i<allCircles.length; i++) {
         var trigger = e.target.dataset.index;
         const circle = document.getElementById(`circle-${trigger}`);
         const circleBg = window.getComputedStyle(circle).backgroundColor;
-        console.log(circleBg);
-        // const circleBgInHex = circleBg.replace('(','').replace(')', '').replace('rgb', '').replace(/,/g, '').split(' ');
-        // FOR HEX VALUES
-        // var hexColor = rgbToHex(parseInt(circleBgInHex[0]), parseInt(circleBgInHex[1]), parseInt(circleBgInHex[2]));
         var inp = document.getElementById('myInput').value = circleBg;
         document.getElementById("inpbtn").click();  
     });
@@ -105,23 +132,15 @@ for(i=0; i<allCircles.length; i++) {
 
 // MENU
 
-const nav = document.querySelector(".navigation");
-const navContainer = document.querySelector(".nav-container");
-const darkSwitch = navContainer.childNodes[3];
-
 function navigationToggle(x) {
     x.classList.toggle("change");
 }
 
 nav.addEventListener("click", e => {
-    // console.log("Clicked");
     navContainer.classList.toggle("hide")
 })
 
 // ADD GRADIENTS 
-
-var addGradient = document.querySelector('.add-gradient')
-var gradientForm = document.querySelector('.add-gradient-form')
 
 function createGradientCard(gname, color1, color2, id) {
     
@@ -163,13 +182,12 @@ addGradient.childNodes[1].addEventListener("click", e => {
 const submitButton = document.querySelector('.submit');
 
 submitButton.childNodes[1].addEventListener("click", e => {
-    console.log("Clicked Submit Button");
 
     const gradientName = document.getElementById('g-name').value;
     const color1 = document.getElementById('color-1').value;
     const color2 = document.getElementById('color-2').value;
     const id = cardContainer.childElementCount + 1;
-    // console.log(gradientName, color1, color2, id);
+
     createGradientCard(gradientName, color1, color2, id);
 })
 
@@ -178,10 +196,7 @@ submitButton.childNodes[1].addEventListener("click", e => {
 
 darkSwitch.addEventListener("click", e => {
 
-    console.log("Clicked")
-
     const sw = navContainer.childNodes[3];
-
     container.classList.toggle("dt-container")
     
     var cards = document.querySelectorAll('.card');
@@ -197,22 +212,60 @@ darkSwitch.addEventListener("click", e => {
         nameOfGradient[i].classList.toggle('dt-name');
     }
 
-    var links = document.querySelectorAll('.link')
+    var links = document.querySelectorAll('link')
 
     for(i=0; i<links.length; i++) {
         links[i].classList.toggle('dt-link')
     }
 
-    var bookmarkTab = document.querySelector('.bookmark-tab')
-    var btCards = document.querySelectorAll('.bookmark-tab-card');
-    var utils = document.querySelector('.utilities');
+    // utils.classList.toggle('dt-bookmarks-outline')
+    // bookmarkTab.classList.toggle('dt-tab');
 
-    utils.classList.toggle('dt-bookmarks-outline')
-    bookmarkTab.classList.toggle('dt-tab');
+    // for(i=0; i<btCards.length; i++) {
+    //     btCards[i].classList.toggle('dt-tab-cards');
+    // }
 
-    for(i=0; i<btCards.length; i++) {
-        btCards[i].classList.toggle('dt-tab-cards');
+    gradientForm.classList.toggle("dt-add-gradient-form");
+    console.log(gradientForm.childNodes);
+    gradientForm.childNodes[1].classList.toggle("dt-gname")   
+    console.log(gradientForm.childNodes[7].childNodes);
+    var inp = gradientForm.childNodes[5].childNodes;
+    console.log(inp);
+    inp[1].classList.toggle("dt-color");
+    inp[5].classList.toggle("dt-color");
+})
+
+// OLDER VERSION SUPPORT
+
+function compatibilty(ele) {
+    if(ele.dataset.comp == "false") {
+        ele.dataset.comp = "true";
+        var comp = document.querySelector(".comp");
+        comp.childNodes[1].textContent = "Older Browser Support Enabled";
+        comp.classList.toggle("hide");
+        setTimeout(function() {
+            comp.classList.toggle("hide");
+        }, 750)
+
+    } else {
+        ele.dataset.comp = "false";
+        var comp = document.querySelector(".comp");
+        comp.childNodes[1].textContent = "Older Browser Support Disabled";
+        comp.classList.toggle("hide");
+        setTimeout(function() {
+            comp.classList.toggle("hide");
+        }, 750)
     }
+}
 
+// BOOKMARKS - COMING SOON
+
+const comingSoonTab = document.querySelector(".coming-soon-tab");
+const bookmarks = utils.childNodes[1];
+bookmarks.addEventListener("click", e => {
+    comingSoonTab.classList.toggle("hide")
+    setTimeout(function() {
+        comingSoonTab.classList.toggle("hide")
+    }, 750)
 })
 
